b7dc7be9c4475f41c4688ea2eaddd8bbc7561102
orhanors <orsorhan1@gmail.com>
2021-01-29 15:40:40 +0300

    bugs fixed

--- src/components/ShowSearchResult/index.js
+++ src/components/ShowSearchResult/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ListGroup } from "react-bootstrap";
+import onClickOutside from "react-onclickoutside";
 
 import { Link } from "react-router-dom";
 import "./styles.scss";
